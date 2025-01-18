e38ce7a811847be7be17b9e77992cc0a404b1b02
souravahmednirob <souravahmednirob@gmail.com>
2025-01-19 01:08:42 +0600

    initial backend commit

---
+++ xenonvita/src/main/java/com/xenonvita/XenonvitaApplication.java
@@ -0,0 +1,13 @@
+package com.xenonvita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XenonvitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XenonvitaApplication.class, args);
+    }
+
+}
