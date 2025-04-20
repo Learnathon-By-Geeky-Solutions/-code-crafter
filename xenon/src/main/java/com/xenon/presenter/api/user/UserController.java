e3cafbd86a8a6dc253475b12afd21eaf52dab15f
souravahmednirob <souravahmednirob@gmail.com>
2025-04-21 02:29:44 +0600

    update user controller service functionality

--- xenon/src/main/java/com/xenon/presenter/api/user/UserController.java
+++ xenon/src/main/java/com/xenon/presenter/api/user/UserController.java
@@ -1,8 +1,11 @@
 package com.xenon.presenter.api.user;
 
 import com.xenon.common.annotation.PreAuthorize;
+import com.xenon.core.domain.request.hospital.CreateAppointmentTableRequest;
 import com.xenon.core.domain.request.user.CreateAccountRequest;
+import com.xenon.core.domain.request.user.UpdateAccountRequest;
 import com.xenon.core.service.user.UserService;
+import com.xenon.data.entity.user.UserRole;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/user")
 @RequiredArgsConstructor
-public class UserResource {
+public class UserController {
 
     private final UserService userService;
 
@@ -21,12 +24,14 @@ public class UserResource {
         return userService.createAccount(body);
     }
 
-    @PutMapping
-    @PreAuthorize
+
+    @PutMapping("user-profile-update")
+    @PreAuthorize(shouldCheckAccountStatus = true)
     @SecurityRequirement(name = "bearerAuth")
-    public ResponseEntity<?> updateProfile() {
-        return userService.update();
+    public ResponseEntity<?> updateProfile(@Nullable  @RequestBody UpdateAccountRequest body) {
+        return userService.update(body);
     }
+
 
     @GetMapping
     @PreAuthorize(shouldCheckAccountStatus = true)
@@ -34,4 +39,5 @@ public class UserResource {
     public ResponseEntity<?> getUserProfile() {
         return userService.getProfile();
     }
+
 }
