80b9a43dab681491ae676ff4c4ae2ed361dede3b
jiaamasum <jiaamasum@gmail.com>
2025-04-29 03:53:45 +0600

    feat: Add backend files from backend-initiallization branch (excluding .idea folder)

---
+++ xenonvita/src/test/java/com/xenonvita/XenonvitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xenonvita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XenonvitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
