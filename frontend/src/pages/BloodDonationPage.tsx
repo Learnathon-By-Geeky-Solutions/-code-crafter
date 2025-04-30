fb1570499462b7d8c7f858647458f3211f02a302
jiaamasum <jiaamasum@gmail.com>
2025-04-30 23:57:06 +0600

    chore: remove frontend, temp_files, and xenonvita folders

--- frontend/src/pages/BloodDonationPage.tsx
+++
@@ -1,9 +0,0 @@
-import { BloodDonationSection } from '../components/features/BloodDonationSection';
-
-export function BloodDonationPage() {
-  return (
-    <div className="container mx-auto">
-      <BloodDonationSection />
-    </div>
-  );
-}
